a962d740f6247f7a45f2849b424de765875cb12d
Michael Soprano <miccighel@gmail.com>
2015-07-07 23:01:04 +0200

    Aggiornata la pagina per la visualizzazione degli score degli studenti

--- views/studentscore.php
+++ views/studentscore.php
@@ -16,33 +16,39 @@ if (isloggedin()) {
 
     $userid = $USER->id;
     $usercontext = context_user::instance($userid);
-
     print_page_attributes('pluginname', 'pluginname', $usercontext, 'local');
 
-    if (has_capability('local/mpa:localoverview', $usercontext, $userid)) {
+    $renderer = $PAGE->get_renderer('local_mpa');
 
-        $renderer = $PAGE->get_renderer('local_mpa');
 
-        $final_students = get_active_students();
+    if (has_capability('local/mpa:studentscore', $usercontext, $userid)) {
 
-        foreach ($final_students as $student) {
-            $stud_prop = $student->getProperties();
-            $submissions = $student->getSubmissions();
-            foreach($submissions as $submission) {
-                $sub_prop = $submission->getProperties();
-                $assessments = $submission->getAssessments();
-                foreach($assessments as $assesment){
-                    $ass_prop = $assesment->getProperties();
-                    $grades = $assesment->getGrades();
-                    foreach($grades as $grade){
-                        $grade_prop = $grade->getProperties();
-                    }
-                }
+        $submissions_data = $DB->get_records_sql('SELECT *
+        FROM {workshop_submissions} AS mws INNER JOIN {workshop_assessments} AS mwa ON mwa.submissionid=mws.id WHERE mwa.reviewerid!=mws.authorid', array());
+
+        foreach ($submissions_data as $submission_data){
+
+            // ID della risoluzione
+            $submissionid = $submission_data->submissionid;
+            // Autore dell'assessment per la risoluzione
+            $evaluatorid = $submission_data->reviewerid;
+            // Autore della risoluzioned
+            $solverid = $submission_data->authorid;
+
+            $temp = $DB->get_records_sql('SELECT * FROM {mpa_submission_data} WHERE id=? AND evaluatorid=? AND solverid=?', array($submissionid,$evaluatorid,$solverid));
+
+
+            if (empty($temp)) {
+                $DB->execute('INSERT INTO {mpa_submission_data} (id,evaluatorid,solverid,submission_steadiness,submission_score,assessment_value,assessment_goodness)
+                    VALUES (?,?,?,?,?,?,?)',$parms=array($submissionid,$evaluatorid,$solverid,null,null,null,null));
+            } else {
+                $DB->execute('UPDATE {mpa_submission_data}
+                    SET id=?, evaluatorid=?, solverid=?, submission_steadiness=?,submission_score=?, assessment_value=?,assessment_goodness=? WHERE id=? AND evaluatorid=? AND solverid=?',
+                    $parms = array($submissionid,$evaluatorid,$solverid,null,null,null,null,$submissionid,$evaluatorid,$solverid));
             }
         }
 
         echo $renderer->render_student_scores();
-
     } else {
         echo $renderer->render_capability_error();
     }
